aea129f5a838dbbab65c2491844d68315b937bd8
Ana Nitescu <AnaMaria.Nitescu@endava.com>
2025-06-02 22:04:24 +0300

    session18

---
+++ src/main/java/configFile/ConfigFile.java
@@ -0,0 +1,21 @@
+package configFile;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+
+public class ConfigFile {
+
+    //generic method to deserialize xml file and returned the desired area
+    public static <T> T createConfigNode(Class<T> klass) {
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(klass);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            return klass.cast(unmarshaller.unmarshal(new File("src/test/resources/configFile/shareDataConfig.xml")));
+        } catch (JAXBException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+}
--- src/main/java/shared/SharedData.java
+++ src/main/java/shared/SharedData.java
@@ -1,11 +1,11 @@
 package shared;
 
+import configFile.ConfigFile;
+import configFile.configNode.ConfigurationNode;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 
 import java.time.Duration;
 
@@ -15,8 +15,11 @@ public class SharedData {
 
     @BeforeMethod
     public void setUpBrowser() {
+        ConfigurationNode configurationNode = ConfigFile.createConfigNode(ConfigurationNode.class);
+
         driver = new ChromeDriver();
-        driver.get("https://demoqa.com/");
+        //driver.get("https://demoqa.com/");
+        driver.get(configurationNode.driverConfigNode.url);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
